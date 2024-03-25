d3162ba2ddf4515f801ad949c7eb44d8ebc05870
lynettenyambura <linetnyambura45@gmail.com>
2024-03-25 11:41:18 +0300

    casepage&pdfdownload

--- newform.js
+++ newform.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch';
 import makeFetchCookie from 'fetch-cookie';
 import fs from 'fs';
+import cheerio from 'cheerio';
 
 const url = 'http://kenyalaw.org/caselaw/cases/advanced_search_courts?court=190000';
 const searchUrl = 'http://kenyalaw.org/caselaw/cases/advanced_search/';
@@ -8,6 +9,10 @@ const filePath = 'formPage.html';
 const searchFilePath = 'searchPage.html';
 const paginationBaseUrl = 'http://kenyalaw.org/caselaw/cases/advanced_search/page/';
 const fetchCookie = makeFetchCookie(fetch);
+
+
+
+
 
 const getFormPage = async () => {
     try {
@@ -36,6 +41,7 @@ const getFormPage = async () => {
         console.error('Error fetching form page:', error);
     }
 };
+
 
 const searchAndParseDate = async () => {
     try {
@@ -70,16 +76,18 @@ const searchAndParseDate = async () => {
             method: 'POST',
             headers: headers,
             body: searchParams,
-            credentials: 'include'
+            
         });
 
         const html = await response.text();
-        fs.writeFileSync(searchFilePath, html);
+        await fs.promises.writeFile(searchFilePath, html);
+        //fs.writeFileSync(searchFilePath, html);
         console.log('Search results saved to file:', searchFilePath);
 
         const paginate = async () => {
-            const totalPages = 40;
-            for (let page = 10; page <= totalPages; page += 10) {
+            const totalPages = 50;
+            for (let page = 10; page <= 50; page += 10) {
+                
                 const pageUrl = `${paginationBaseUrl}/${page}/`;
                 try {
                     const pageResponse = await fetchCookie(pageUrl, {
@@ -90,33 +98,151 @@ const searchAndParseDate = async () => {
                             'Accept-Language': 'en-US,en;q=0.9',
                             'Cache-Control': 'no-cache',
                             'Connection': 'keep-alive',
-                            'Cookie': '_gid=GA1.2.1841360343.1710683877; _ga=GA1.2.1357335161.1708850195; _ga_SCV6JN18JH=GS1.1.1710767592.5.0.1710767598.0.0.0; cisession=a%3A4%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%228ea635368552e5c4e0e694483a0f9696%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A15%3A%22192.168.100.101%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A111%3A%22Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F122.0.0.0+Safari%2F537.36%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1710770964%3B%7Dc3c3e44b1875a846d3df2bffd7127c0e; _gat=1',
-                            'Host': 'kenyalaw.org',
+                           'Host': 'kenyalaw.org',
                             'Pragma': 'no-cache',
                             'Referer': 'http://kenyalaw.org/caselaw/cases/advanced_search_courts?court=190000',
                             'Upgrade-Insecure-Requests': '1',
                             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
                         },
-                        credentials: 'include'
+                        
                     });
         
-                    if (pageResponse.ok) {
+                    if (pageResponse.status === 200) {
                         const pageHtml = await pageResponse.text();
                         const pageFilePath = `page_${page}.html`;
-                        fs.writeFileSync(pageFilePath, pageHtml);
+                        //fs.writeFileSync(pageFilePath, pageHtml);
+                        await fs.promises.writeFile(pageFilePath, pageHtml);
                         console.log(`Page ${page} content saved to file:`, pageFilePath);
                     } else {
                         console.error('Error fetching page:', pageResponse.statusText);
                     }
                 } catch (error) {
-                    console.error('Error fetching page:', error);
+                     console.error('Error fetching page:', error);
                 }
             }
         };
-        paginate(); 
+        await paginate(); 
+        await readmore();
     } catch (error) {
         console.error('Error in searchAndParseDate:', error);
     }
 };
 
-searchAndParseDate(); 
+// function to download the pdf file
+const downloadPDF = async (pdfUrl, caseId) => {
+    try {
+    const response = await fetchCookie(pdfUrl, {
+        method: 'GET',
+        headers: {
+            'authority': 'kenyalaw.org',
+           'scheme': 'https',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Referer': 'https://kenyalaw.org/caselaw/cases/view/227586/',
+            'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+            'Sec-Ch-Ua-Mobile': '?1',
+            'Sec-Ch-Ua-Platform': 'Android',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'
+
+        },
+        credentials: 'same-origin'
+    });
+    if (response.status === 200) {
+        const pdfData = await response.buffer(); 
+        const pdfFileName = `${caseId}.pdf`; 
+        fs.writeFileSync(pdfFileName, pdfData); 
+        console.log(`PDF file saved: ${pdfFileName}`);
+    } else {
+        console.error('Error downloading PDF:', response.statusText);
+    }
+} catch (error) {
+    console.error('Error downloading PDF:', error);
+}
+}
+
+
+// parsing results to click readmore for each case/document
+const readmore = async () => {
+    try {
+    const urls = [];
+    const totalPages = 50;
+    for (let page = 10; page <= 50; page += 10) {
+        const pageFilePath = `page_${page}.html`;
+        const pageHtml = await fs.promises.readFile(pageFilePath, 'utf8');
+        
+        const $= cheerio.load(pageHtml);
+        $('.post').each( function() {
+            const href = $(this).find('.show-more').attr('href');
+            if (href && href.includes('/caselaw/cases/view/')) {
+                
+                urls.push(href);
+            }
+        });
+    
+        
+    };
+    await fs.promises.writeFile('urls.json',JSON.stringify(urls,null,2))
+    
+    
+    for(const url of urls) {
+        const pageUrl = await url
+       const id = pageUrl.split('/')[6];
+
+        const response = await fetchCookie(pageUrl, {
+
+            method: 'GET',
+            headers: {
+                'authority': 'kenyalaw.org',
+                'scheme': 'https',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Cache-Control': 'no-cache',
+                'Pragma': 'no-cache',
+                'Referer': 'https://kenyalaw.org/caselaw/cases/advanced_search/',
+                'Sec-Ch-Ua':' "Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+                'Sec-Ch-Ua-Mobile': '?0',
+                'Sec-Ch-Ua-Platform': "Windows",
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+            },
+            credentials: 'same-origin'
+ 
+        }); 
+            if (response.status === 200) {
+                const html = await response.text();
+                const caseFilePath = `${id}.html`;
+                await fs.promises.writeFile(`./links/${caseFilePath}`, html);
+                console.log('Case content saved to file:', caseFilePath);
+
+                // Fetch pdf and save it
+                const pdfUrl = `https://kenyalaw.org/caselaw/cases/export/${id}/pdf`;
+                downloadPDF(pdfUrl, id);
+            } else {
+                console.error('Error fetching case:', response.statusText);
+            }
+        }
+        
+    } catch (error) {
+        console.error('Error in readmore:', error);
+    }
+
+    
+
+    // return pageHtml
+};
+searchAndParseDate();
+readmore()
